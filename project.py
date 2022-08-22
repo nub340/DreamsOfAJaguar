7c0c5ab3bcbfd164cdbee40a88ae13c207041539
nub340 <nkoier@gmail.com>
2022-08-22 14:47:54 -0700

    Game class

--- project.py
+++ project.py
@@ -1,6 +1,33 @@
 
+import pygame, sys
+
+WIDTH = 800
+HEIGTH = 600
+FPS = 60
+
+class Game:
+	def __init__(self):
+		  
+		# general setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		pygame.display.set_caption('Mayan Tower Defense (CS50P Final)')
+		self.clock = pygame.time.Clock()
+	
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+
+			self.screen.fill('black')
+			pygame.display.update()
+			self.clock.tick(FPS)
+
 def main():
-    print("Hello World!!!")
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
     main()
